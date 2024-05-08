7110c1a5c19abdef1f66b61b28c530e79932d7cc
Anton Curanz <anton@curanz.de>
2024-05-08 11:57:21 +0200

    tiny11 vm

--- modules/virtualization/libvirt.nix
+++ modules/virtualization/libvirt.nix
@@ -20,11 +20,13 @@ in
       "qemu:///system" = {
         domains = [
           { definition = ./domains/nixos.xml; }
+          { definition = ./domains/win11.xml; }
         ];
         pools = [{
           definition = ./pool.xml;
           volumes = [
             { definition = ./volumes/nixos.xml; }
+            { definition = ./volumes/win11.xml; }
           ];
         }];
       };
