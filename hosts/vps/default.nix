19ba4c17957ad06f654f41020f3d2046b28bb024
Anton Curanz <anton@curanz.de>
2024-05-08 07:58:37 +0200

    vps success

--- hosts/vps/default.nix
+++ hosts/vps/default.nix
@@ -15,13 +15,30 @@
   };
 
   # Configure network IP and DNS
-  systemd.network.networks.ens3 = {
-    address = ["${secrets.vps.networking.ip}"];
-    gateway = ["${secrets.vps.networking.gateway}"];
+  #systemd.network.networks.ens3 = {
+  #  address = ["${secrets.vps.networking.ip}"];
+  #  gateway = ["${secrets.vps.networking.gateway}"];
+  #};
+
+  boot.zfs.devNodes = "/dev/disk/by-path";
+
+  networking = {
+    interfaces.ens3 = {
+      ipv4.addresses = [{
+        address = "${secrets.vps.networking.ip}";
+        prefixLength = 24;
+      }];
+      ipv6.addresses = [{
+        address = "${secrets.vps.networking.ip6}";
+        prefixLength = 64;
+      }];
+    };
+    defaultGateway.address = "${secrets.vps.networking.gateway}";
+    defaultGateway6.address = "${secrets.vps.networking.gateway6}";
+    nameservers = [
+      "${secrets.vps.networking.dns}"
+    ];
   };
-  networking.nameservers = [
-    "${secrets.vps.networking.dns}"
-  ];
 
   # modules
   impermanence.enable = true;
@@ -30,9 +47,18 @@
   networking.hostId = "af1cdddd";
   system.stateVersion = "23.11";
 
-  # hardware
-  boot.initrd.availableKernelModules = [ "ahci" "xhci_pci" "virtio_pci" "virtio_scsi" "sr_mod" "virtio_blk" ];
-  boot.kernelModules = [ "kvm-intel" ];
+  # hardware (including qemu-guest.nix)
+  boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "sr_mod" "virtio_net" "virtio_pci" "virtio_mmio" "virtio_blk" "virtio_scsi" "9p" "9pnet_virtio" ];
+  boot.initrd.kernelModules = [ "virtio_balloon" "virtio_console" "virtio_rng" ];
+  boot.kernelModules = [ "kvm-amd" ];
+
+  boot.initrd.postDeviceCommands = lib.mkIf (!config.boot.initrd.systemd.enable)
+    ''
+      # Set the system time from the hardware clock to work around a
+      # bug in qemu-kvm > 1.5.2 (where the VM clock is initialised
+      # to the *boot time* of the host).
+      hwclock -s
+    '';
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 }
