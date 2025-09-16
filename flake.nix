3a513a5a6cb00ae03b91744511cd8beca79bfafe
Anton Curanz <anton@curanz.de>
2025-09-16 18:54:30 +0200

    tahoe iterm2

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/release-25.05";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs-unstable.url = "github:antoncuranz/nixpkgs/release-25.05";
 
     impermanence.url = "github:nix-community/impermanence";
     arkenfox.url = "github:dwarfmaster/arkenfox-nixos";
