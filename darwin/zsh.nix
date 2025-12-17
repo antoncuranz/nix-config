257d1da5488c56eb52adb6029a07abeb401bbd03
Anton Curanz <anton.curanz@maibornwolff.de>
2025-12-17 17:09:30 +0100

    ghostty fix

--- darwin/zsh.nix
+++ darwin/zsh.nix
@@ -8,6 +8,9 @@
 
       # Make user colour green in prompt instead of default blue
       zstyle ':prompt:grml:left:items:user' pre '%F{green}%B'
+      if [[ "$TERM_PROGRAM" == "ghostty" ]]; then
+          export TERM=xterm-256color
+      fi
     '';
     promptInit = ""; # otherwise it'll override the grml prompt
   };
